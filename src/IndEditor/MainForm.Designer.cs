2a3af30b88633e84dc52a2f10eec33c833b6ec4e
scorpio21 <scorpio21@users.noreply.github.com>
2026-08-13 01:50:35 +0200

    Agregar icono al formulario desde recursos

--- src/IndEditor/MainForm.Designer.cs
+++ src/IndEditor/MainForm.Designer.cs
@@ -6,6 +6,7 @@ partial class MainForm
 {
     private void InitializeComponent()
     {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
         SuspendLayout();
         // 
         // MainForm
@@ -13,6 +14,7 @@ partial class MainForm
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(1150, 700);
+        Icon = (Icon)resources.GetObject("$this.Icon");
         Name = "MainForm";
         ResumeLayout(false);
     }
